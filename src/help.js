f4b0838ceb06017b14d0f7629f57212b41f89175
SENPAI-BOT-1 <80009152+SENPAI-BOT-1@users.noreply.github.com>
2021-03-10 03:52:04 -0400

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,177 @@
+const help = (prefix) => { 
+	return `   
+
+♞「 *BOT P R I N C E* 」♞
+
+♘ *informações*
+  ♞ Prefix: 「  *  」
+  ♞ Criador : Senpai
+
+ *se o bot não responder conte até 10 e tente novamente*
+  mais informações:
+👇
+https://wa.me/5565981709268
+
+Canal do criador: https://youtube.com/channel/UC1GWxdNnpB0oXT9bDUUWJ7w
+
+♞ *SOBRE*
+  │
+  ├─ ♘ *info
+  ├─ ♘ *blocklist
+  ├─ ♘ *chatlist
+  ├─ ♘ *ping
+  └─ ♘ *bugreport
+◪ *FAZER*
+  │
+  ├─ ♘ *sticker (faz figurinha animada e normal)
+  ├─ ♘ *stickergif (faz figurinha animada a msm coisa de cima)
+  ├─ ♘ *toimg (converte Sticker em imagens)
+  ├─ ♘ *tomp3
+  ├─ ♘ *bpink
+  ├─ ♘ *marvellogo
+  ├─ ♘ *snowwrite
+  ├─ ♘ *3dtext
+  ├─ ♘ *ninjalogo
+  ├─ ♘ *water
+  ├─ ♘ *firetext
+  ├─ ♘ *logowolf
+  ├─ ♘ *logowolf2
+  ├─ ♘ *phlogo
+  ├─ ♘ *glitch
+  ├─ ♘ *neonlogo
+  ├─ ♘ *neonlogo2
+  ├─ ♘ *lionlogo
+  ├─ ♘ *jokerlogo
+  ├─ ♘ *shadow
+  ├─ ♘ *burnpaper
+  ├─ ♘ *coffee
+  ├─ ♘ *lovepaper
+  ├─ ♘ *woodblock
+  ├─ ♘ *qowheart
+  ├─ ♘ *mutgrass
+  ├─ ♘ *undergocean
+  ├─ ♘ *woodenboards
+  ├─ ♘ *wolfmetal
+  ├─ ♘ *metalictglow
+  ├─ ♘ *8bit
+  ├─ ♘ *ttp
+  ├─ ♘ *herrypotter
+  ├─ ♘ *pubglogo
+  └─ ♘ *quotemaker
+♞ *MEDIA*
+  │
+  ├─ ♘ *trendtwit
+  ├─ ♘ *randomkpop
+  └─ ♘ *ytsearch
+♞ *EDUCATION*
+  │
+  ├─ ♘ *wiki
+  ├─ ♘ *wikien
+  ├─ ♘ *nulis
+  ├─ ♘ *quotes
+  ├─ ♘ *quotes2
+  └─ ♘ *artinama
+♞ *ESCUDOS MÁGICOS*
+  │
+  ├─ ♘ *apakah
+  ├─ ♘ *kapankah
+  ├─ ♘ *rate
+  └─ ♘ *bisakah
+♞ *DOWNLOADER*
+  │
+  ├─ ♘ *images
+  ├─ ♘ *ytmp3
+  ├─ ♘ *ytmp4
+  ├─ ♘ *tiktok
+  └─ ♘ *joox
+◪ *MEME*
+  │
+  ├─ ♘ *meme
+  └─ ♘ *memeindo
+◪ *SOM*
+  │
+  ├─ ♘ *play (abaixa música, só funciona se o título tiver certo)
+  └─ ♘ *tts (converte texto em áudio) mó legal ksksks
+♞ *MÚSICA*
+  │
+  ├─ ♘ *lirik
+  └─ ♘ *chord
+♞ *ISLAM*
+  │
+  └─ ♘ *quran
+♞ *STALK*
+  │
+  ├─ ♘ *tiktokstalk
+  └─ ♘ *igstalk
+♞ *NSFW*
+  │
+  ├─ ♘ *nsfwbobs
+  ├─ ♘ *randomhentai
+  ├─ ♘ *nsfwtrap
+  ├─ ♘ *nsfwass
+  ├─ ♘ *nsfwbelly
+  ├─ ♘ *nsfwsidebobs
+  ├─ ♘ *nsfwahegao
+  ├─ ♘ *nsfwthighs
+  ├─ ♘ *nsfwarmpits
+  └─ ♘ *nsfwfeets
+◪ *WIBU*
+  │
+  ├─ ♘ *neonime
+  ├─ ♘ *pokemon
+  ├─ ♘ *loli (foto de loli😏) *as vezes não funciona*
+  ├─ ♘ *waifu
+  ├─ ♘ *randomanime
+  ├─ ♘ *husbu
+  ├─ ♘ *husbu2
+  ├─ ♘ *wait
+  └─ ♘ *nekonime
+♞ *DIVERSÃO*
+  │
+  ├─ ♘ *alay
+  ├─ ♘ *gantengcek
+  ├─ ♘ *watak
+  ├─ ♘ *hobby
+  ├─ ♘ *game
+  ├─ ♘ *bucin
+  ├─ ♘ *trust
+  ├─ ♘ *dare
+  └─ ♘ *simi
+♞ *INFORMAÇÃO*
+  │
+  ├─ ♘ *bahasa
+  ├─ ♘ *kodenegara
+  ├─ ♘ *kbbi
+  ├─ ♘ *fakta
+  ├─ ♘ *infocuaca
+  ├─ ♘ *infogempa
+  ├─ ♘ *jadwaltvnow
+  └─ ♘ *covid
+◪ *DONO*
+  │
+  ├─ ♘ *setprefix
+  ├─ ♘ *block
+  ├─ ♘ *bc
+  ├─ ♘ *bcgc
+  ├─ ♘ *clone
+  └─ ♘ *clearall
+♞ *OUTROS*
+  │
+  ├─ ♘ *send
+  ├─ ♘ *wame
+  ├─ ♘ *exe
+  ├─ ♘ *qrcode
+  ├─ ♘ *afk
+  ├─ ♘ *timer
+  ├─ ♘ *fml
+  ├─ ♘ *tagall
+  ├─ ♘ *tagallink
+  └─ ♘ *fml2
+ᬊ͜͡𝑺𝚯𝑳𝑫𝑨͢𝑫𝚯𝑺⛧𝑹𝑪𝑻ᬏ᭄⇝͜͡🇭🇲x➌➐
+https://chat.whatsapp.com/JTLIzLvS5M4JQp6lS5Idnf
+MEU CHAT BOTS
+https://chat.whatsapp.com/Kr5QiYOLrlWCsIhfDz0vnG
+BOT By:
+`
+}
+exports.help = help
